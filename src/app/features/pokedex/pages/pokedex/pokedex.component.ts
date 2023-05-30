6f1f8ca2aa73b93b9e9ee1293861cf6237895284
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-05-30 21:08:30 +0100

    simplified calls of pokedex service in pokedex component, moving logic into getPokedex function

--- src/app/features/pokedex/pages/pokedex/pokedex.component.ts
+++ src/app/features/pokedex/pages/pokedex/pokedex.component.ts
@@ -31,14 +31,19 @@ export class PokedexComponent implements OnInit {
   ) {}
 
   isMenuCollapsed = true;
-  pokemonEntries: PokemonEntry[] = {} as PokemonEntry[];
-  model: PokemonEntry = {
-    entry_number: 0,
-    pokemon_species: {
-      name: '',
-      url: '',
-    },
-  };
+
+  pokedex: Pokedex = {} as Pokedex;
+  loadedPokemonEntries: PokemonEntry[] = [] as PokemonEntry[];
+
+  pokedexNumber: number = 1;
+  pageNumber: number = 1;
+  totalPages: number = 0;
+  offset: number = 50;
+
+  pokedexEntrySearch: PokemonEntry[] = {} as PokemonEntry[];
+  model: PokemonEntry = {} as PokemonEntry;
+  titleCasePipe = new TitleCasePipe();
+
   formatter = (pokemon: PokemonEntry) => pokemon.pokemon_species.name;
   search: OperatorFunction<
     string,
@@ -49,7 +54,7 @@ export class PokedexComponent implements OnInit {
       distinctUntilChanged(),
       filter((term) => term.length >= 2),
       map((term) =>
-        this.pokemonEntries
+        this.pokedexEntrySearch
           .filter((pokemon) =>
             new RegExp(term, 'mi').test(pokemon.pokemon_species.name)
           )
@@ -57,26 +62,24 @@ export class PokedexComponent implements OnInit {
       )
     );
 
-  pokedex: Pokedex = {
-    descriptions: [],
-    id: 0,
-    is_main_series: false,
-    name: '',
-    names: [],
-    pokemon_entries: [],
-    region: {},
-    version_groups: [],
-  };
+  ngOnInit(): void {
+    this.getAllPokemon();
+    this.route.paramMap.subscribe((params) => {
+      this.pokedexNumber = Number(params.get('id')) || 1;
+      this.route.queryParamMap.subscribe((params) => {
+        this.pageNumber = Number(params.get('page')) || 1;
+        this.getPokedex(this.pageNumber, this.pokedexNumber);
+      });
+    });
+  }
 
-  pokedexNumber: number = 1;
-  pageNumber: number = 1;
-  totalPages: number = 0;
-  offset: number = 100;
-  titleCasePipe = new TitleCasePipe();
+  onScroll(): void {
+    this.getPokedex(++this.pageNumber, this.pokedexNumber);
+  }
 
   getAllPokemon(): void {
     this.pokedexService.getPokedex(1).subscribe((pokedex: Pokedex) => {
-      this.pokemonEntries = pokedex.pokemon_entries.map((entry) => {
+      this.pokedexEntrySearch = pokedex.pokemon_entries.map((entry) => {
         // Capitalize the first letter of each word in a Pokemon's name
         return {
           entry_number: entry.entry_number,
@@ -96,56 +99,29 @@ export class PokedexComponent implements OnInit {
     this.pokedexService
       .getPokedex(pokedexNumber)
       .subscribe((pokedex: Pokedex) => {
-        this.pokedex.pokemon_entries = pokedex.pokemon_entries.slice(
-          startIndex,
-          endIndex
-        );
-        this.pokedex.pokemon_entries.forEach((pokemon: PokemonEntry) => {
-          let pokemonID = pokemon.pokemon_species.url
-            .split('/')
-            .slice(-2, -1)[0];
-          this.getPokemonSpeciesDetails(pokemonID, pokemon.pokemon_species);
-          this.getPokemonDetails(pokemonID, pokemon.pokemon_species);
-        });
-      });
-  }
-
-  getPokemonSpeciesDetails(
-    pokemonID: string,
-    pokemonSpecies: PokemonSpecies
-  ): void {
-    this.pokedexService
-      .getPokemonSpeciesDetails(pokemonID)
-      .subscribe((pokemonSpeciesDetails: PokemonSpeciesDetails) => {
-        pokemonSpecies.species_details = pokemonSpeciesDetails;
-      });
-  }
-
-  getPokemonDetails(pokemonID: string, pokemonSpecies: PokemonSpecies): void {
-    this.pokedexService
-      .getPokemonDetails(pokemonID)
-      .subscribe((details: PokemonDetails) => {
-        pokemonSpecies.details = details;
+        pokedex.pokemon_entries
+          .slice(startIndex, endIndex)
+          .forEach((pokemon: PokemonEntry) => {
+            let pokemonID = pokemon.pokemon_species.url
+              .split('/')
+              .slice(-2, -1)[0];
+            this.pokedexService
+              .getPokemonSpeciesDetails(pokemonID)
+              .subscribe((pokemonSpeciesDetails: PokemonSpeciesDetails) => {
+                pokemon.pokemon_species.species_details = pokemonSpeciesDetails;
+              });
+            this.pokedexService
+              .getPokemonDetails(pokemonID)
+              .subscribe((details: PokemonDetails) => {
+                pokemon.pokemon_species.details = details;
+              });
+            this.loadedPokemonEntries.push(pokemon);
+          });
       });
   }
 
   onSubmit(): void {
     let pokemonID = this.model.pokemon_species.url.split('/').slice(-2, -1)[0];
     this.router.navigate(['/pokemon/', pokemonID]);
-  }
-
-  ngOnInit(): void {
-    this.getAllPokemon();
-    this.route.paramMap.subscribe((params) => {
-      this.pokedexNumber = Number(params.get('id')) || 1;
-      this.route.queryParamMap.subscribe((params) => {
-        this.pageNumber = Number(params.get('page')) || 1;
-        this.getPokedex(this.pageNumber, this.pokedexNumber);
-      });
-    });
-  }
-
-  onScroll(): void {
-    this.getPokedex(++this.pageNumber, this.pokedexNumber);
   }
 }
