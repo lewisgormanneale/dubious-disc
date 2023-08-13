0a23ea056ae4421d59512c0aa02695fcb460c988
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-08-13 20:01:49 +0100

    installed vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+inject();
