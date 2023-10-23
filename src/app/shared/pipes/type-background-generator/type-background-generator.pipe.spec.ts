81179a29ce9c5143e4f18b29d45598ec7b999318
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-10-23 10:45:49 +0100

    darker type backgrounds, borders for pokemon data elements

--- src/app/shared/pipes/type-background-generator/type-background-generator.pipe.spec.ts
+++ src/app/shared/pipes/type-background-generator/type-background-generator.pipe.spec.ts
@@ -11,7 +11,7 @@ describe('TypeBackgroundGeneratorPipe', () => {
 
   it('should return the first type color when no types are passed in', () => {
     const result = pipe.transform([]);
-    expect(result).toEqual(AllPokemonTypeValues[0].type_color);
+    expect(result).toEqual(AllPokemonTypeValues[0].darker_type_color);
   });
 
   it('should return the correct type color for a single type', () => {
@@ -23,7 +23,7 @@ describe('TypeBackgroundGeneratorPipe', () => {
     ];
     const result = pipe.transform(types);
     const typeNumber1 = +types[0].type.url.split('/').slice(-2, -1)[0];
-    expect(result).toEqual(AllPokemonTypeValues[typeNumber1].type_color);
+    expect(result).toEqual(AllPokemonTypeValues[typeNumber1].darker_type_color);
   });
 
   it('should return the correct gradient for two types', () => {
@@ -40,7 +40,7 @@ describe('TypeBackgroundGeneratorPipe', () => {
     const result = pipe.transform(types);
     const typeNumber1ID = +types[0].type.url.split('/').slice(-2, -1)[0];
     const typeNumber2ID = +types[1].type.url.split('/').slice(-2, -1)[0];
-    const expectedColor = `linear-gradient(to bottom right, ${AllPokemonTypeValues[typeNumber1ID].darker_type_color}, ${AllPokemonTypeValues[typeNumber2ID].type_color})`;
+    const expectedColor = `linear-gradient(to bottom right, ${AllPokemonTypeValues[typeNumber1ID].darker_type_color}, ${AllPokemonTypeValues[typeNumber2ID].darker_type_color})`;
     expect(result).toEqual(expectedColor);
   });
 
