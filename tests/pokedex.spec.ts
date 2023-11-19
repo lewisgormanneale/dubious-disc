dfaa8a99e6c8c75c0e7e7a22edb56d2960c925af
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-11-19 22:40:13 +0000

    simplifying playwright test to debug issues

--- tests/pokedex.spec.ts
+++ tests/pokedex.spec.ts
@@ -1,26 +1,13 @@
-import { test, expect, type Page } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-const testData = require('./test-data.json');
-
-test.beforeEach(async ({ page }) => {
+test('When the user is on the pokedex page', async ({ page }) => {
   await page.goto('/pokedex');
-  await page.route(
-    'https://pokeapi.co/api/v2/version-group?limit=50',
-    (route) =>
-      route.fulfill({
-        status: 200,
-        body: testData,
-      })
-  );
-});
-
-test.describe('Given a user is looking for information on Reshiram from Pokémon Black 2 / White 2', () => {
-  test('When the user is on the pokedex page', async ({ page }) => {
-    test.step('Then the user should be presented with links to pokédexes for all available games', async () => {
-      const pokedexLinks = page.getByTestId(
-        'pokedex-version-group-select-item'
-      );
-      expect(await pokedexLinks.count()).toBeGreaterThan(21);
-    });
-  });
+  expect(page.url()).toBe('http://localhost:4200/pokedex');
+  const header = page.getByText('Select Pokédex Version');
+  await expect(header).toBeVisible();
+  await expect
+    .poll(async () =>
+      page.locator('.pokedex-version-group-select-item').count()
+    )
+    .toBeGreaterThanOrEqual(22);
 });
