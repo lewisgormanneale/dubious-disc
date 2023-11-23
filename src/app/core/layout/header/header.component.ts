3bc297a0b82e929dad60cdab2bb44510d3e4a62b
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-11-23 22:15:56 +0000

    mocking out header dropdown options

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CoreModule } from 'src/app/core/core.module';
 import { CommonModule } from '@angular/common';
+import { title } from 'process';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +10,77 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CoreModule, CommonModule, RouterLink],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuItems = [
+    {
+      label: 'Resources',
+      options: [
+        {
+          sectionTitle: 'Databases',
+          options: [
+            {
+              label: 'Pokédex',
+              link: '/pokedex',
+            },
+            {
+              label: 'Team Builder',
+              link: '/',
+            },
+          ],
+        },
+        {
+          sectionTitle: 'Tools',
+          options: [
+            {
+              label: 'Team Builder',
+              link: '/',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Games',
+      options: [
+        {
+          sectionTitle: 'Generation IX',
+          options: [
+            {
+              label: 'Pokémon Scarlet & Violet',
+              link: '/scarlet-violet',
+            },
+          ],
+        },
+        {
+          sectionTitle: 'Generation I',
+          options: [
+            {
+              label: 'Pokémon Red & Blue',
+              link: '/red-blue',
+            },
+            {
+              label: 'Pokémon Yellow',
+              link: '/yellow',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      label: 'Other',
+      options: [
+        {
+          sectionTitle: 'Community',
+          options: [
+            {
+              label: 'About/Contact Us',
+              link: '/about',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  constructor() {}
+}
