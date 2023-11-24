01a324e7bbeb69f04af4fdbaf0a8055bcc5eabb0
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-11-24 14:34:51 +0000

    screen-width header navitems

--- src/app/core/models/navigation.model.ts
+++ src/app/core/models/navigation.model.ts
@@ -54,6 +54,113 @@ export const headerNavItems: NavigationItem[] = [
         ],
       },
       {
+        sectionTitle: 'Generation VIII',
+        options: [
+          {
+            label: 'Pokémon Sword & Shield',
+            link: '/sword-shield',
+          },
+          {
+            label: 'Pokémon Brilliant Diamond & Shining Pearl',
+            link: '/brilliant-diamond-shining-pearl',
+          },
+          {
+            label: 'Pokémon Legends Arceus',
+            link: '/legends-arceus',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation VII',
+        options: [
+          {
+            label: 'Pokémon Sun & Moon',
+            link: '/sun-moon',
+          },
+          {
+            label: 'Pokémon Ultra Sun & Ultra Moon',
+            link: '/ultra-sun-ultra-moon',
+          },
+          {
+            label: "Pokémon Let's Go Pikachu & Eevee",
+            link: '/lets-go',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation VI',
+        options: [
+          {
+            label: 'Pokémon X & Y',
+            link: '/x-y',
+          },
+          {
+            label: 'Pokémon Omega Ruby & Alpha Sapphire',
+            link: '/omega-ruby-alpha-sapphire',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation V',
+        options: [
+          {
+            label: 'Pokémon Black & White',
+            link: '/black-white',
+          },
+          {
+            label: 'Pokémon Black 2 & White 2',
+            link: '/black-2-white-2',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation IV',
+        options: [
+          {
+            label: 'Pokémon Diamond & Pearl',
+            link: '/diamond-pearl',
+          },
+          {
+            label: 'Pokémon Platinum',
+            link: '/platinum',
+          },
+          {
+            label: 'Pokémon HeartGold & SoulSilver',
+            link: '/heartgold-soulsilver',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation III',
+        options: [
+          {
+            label: 'Pokémon Ruby & Sapphire',
+            link: '/ruby-sapphire',
+          },
+          {
+            label: 'Pokémon FireRed & LeafGreen',
+            link: '/firered-leafgreen',
+          },
+          {
+            label: 'Pokémon Emerald',
+            link: '/emerald',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Generation II',
+        options: [
+          {
+            label: 'Pokémon Gold & Silver',
+            link: '/gold-silver',
+          },
+          {
+            label: 'Pokémon Crystal',
+            link: '/crystal',
+          },
+        ],
+      },
+      {
         sectionTitle: 'Generation I',
         options: [
           {
@@ -70,7 +177,30 @@ export const headerNavItems: NavigationItem[] = [
   },
   {
     label: 'Videos',
-    sections: [],
+    sections: [
+      {
+        sectionTitle: 'Watch Online',
+        options: [
+          {
+            label: 'Video Hub',
+            link: '/videos',
+          },
+        ],
+      },
+      {
+        sectionTitle: 'Series',
+        options: [
+          {
+            label: 'Paldean Winds',
+            link: '/paldean-winds',
+          },
+          {
+            label: 'Twilight Wings',
+            link: '/twilight-wings',
+          },
+        ],
+      },
+    ],
   },
   {
     label: 'Other',
