c300b89a8511dc7a7e45e6ec172fe529f3f512fd
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-11-24 16:00:44 +0000

    better management of open dropdowns (no hover support yet)

--- src/app/shared/components/header-dropdown-item/header-dropdown-item.component.ts
+++ src/app/shared/components/header-dropdown-item/header-dropdown-item.component.ts
@@ -1,10 +1,55 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  Input,
+  ElementRef,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { Subscription } from 'rxjs';
 import { NavigationItem } from 'src/app/core/models';
+import { HeaderDropdownItemService } from './header-dropdown-item.service';
 @Component({
   selector: 'app-header-dropdown-item',
   templateUrl: './header-dropdown-item.component.html',
 })
-export class HeaderDropdownItemComponent {
-  @Input()
-  navItem!: NavigationItem;
+export class HeaderDropdownItemComponent implements OnInit, OnDestroy {
+  @Input() navItem!: NavigationItem;
+  isHovered = false;
+  isOpen = false;
+  private subscription: Subscription = new Subscription();
+
+  constructor(
+    private headerDropdownItemService: HeaderDropdownItemService,
+    private elementRef: ElementRef
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.headerDropdownItemService
+      .getOpenDropdown()
+      .subscribe((label) => {
+        if (label !== this.navItem.label) {
+          this.isOpen = false;
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+
+    if (this.isOpen) {
+      this.headerDropdownItemService.setOpenDropdown(this.navItem.label);
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeDropdown(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+  }
 }
