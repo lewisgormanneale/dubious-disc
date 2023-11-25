f6509cf876875ffb5472199643d575121481f6af
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-11-25 23:30:16 +0000

    quick links implemented

---
+++ src/app/features/home/components/quick-link/quick-link.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SanityService } from 'src/app/core/services/sanity.service';
+
+@Component({
+  selector: 'app-quick-link',
+  templateUrl: './quick-link.component.html',
+})
+export class QuickLinkComponent implements OnInit {
+  @Input() url: string = '';
+  @Input() imageUrl: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { CoreModule } from 'src/app/core/core.module';
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { CommonModule } from '@angular/common';
+import { QuickLinkComponent } from './components/quick-link/quick-link.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, QuickLinkComponent],
   imports: [CommonModule, CoreModule, SharedModule, AppRoutingModule],
 })
 export class HomeModule {}
