24abb5a13416127841fce15a4efc0741c3df8c43
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-12-17 13:28:54 +0000

    supabase local deployment test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  production: false,
+  supabaseUrl: 'https://ghqcgxobyvkaqyagxoqg.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdocWNneG9ieXZrYXF5YWd4b3FnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDI3Mzg5NzksImV4cCI6MjAxODMxNDk3OX0.j243cyUqgCZZaXYtteW0rcdEDX9CtIAr9xxuA26rAn8',
   apiUrl: 'https://pokeapi.co/api/v2/',
 };
