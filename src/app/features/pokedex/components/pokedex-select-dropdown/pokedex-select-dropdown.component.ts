28eef8eb261f602cb20718f1e142171f8108e226
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-01-06 15:54:40 +0000

    return only available pokedexes for a pokemon

--- src/app/features/pokedex/components/pokedex-select-dropdown/pokedex-select-dropdown.component.ts
+++ src/app/features/pokedex/components/pokedex-select-dropdown/pokedex-select-dropdown.component.ts
@@ -8,27 +8,21 @@ import { SupabaseService } from 'src/app/core/services/supabase.service';
   templateUrl: './pokedex-select-dropdown.component.html',
 })
 export class PokedexSelectDropdownComponent {
-  @Input() selectedPokemonSpeciesId: number = 0;
-  @Input() selectedPokemonSpeciesIdentifier: string = '';
+  @Input() selectedPokemonSpecies?: Tables<'pokemon_species'>;
   @Input() currentVersionGroupName: string = '';
 
-  public sections: DropdownLinkSection[] = [
-    {
-      name: 'General',
-      options: [
-        {
-          name: 'National',
-          path: '/pokedex/national',
-        },
-      ],
-    },
-  ];
+  public sections: DropdownLinkSection[] = [];
+
   public placeholder: string = 'Select Pokedex';
 
   private supabase: SupabaseService = inject(SupabaseService);
 
   ngOnInit(): void {
-    this.getAllPokedexVersionGroups();
+    if (this.selectedPokemonSpecies) {
+      this.getPokedexVersionGroupsWithPokemon();
+    } else {
+      this.getAllPokedexVersionGroups();
+    }
   }
 
   getAllPokedexVersionGroups(): void {
@@ -38,7 +32,7 @@ export class PokedexSelectDropdownComponent {
       pokedexVersionGroups: this.supabase.getAllPokedexVersionGroups(),
     }).subscribe(({ versionGroups, generations, pokedexVersionGroups }) => {
       const versionGroupIds = versionGroups.map((item: any) => item.id);
-      const filteredPokedexVersionGroups = pokedexVersionGroups.filter(
+      let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
         (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
           versionGroupIds.includes(pokedexVersionGroup.version_group_id)
       );
@@ -46,7 +40,7 @@ export class PokedexSelectDropdownComponent {
         filteredPokedexVersionGroups.map((item) => item.version_group_id)
       );
 
-      this.sections.push(
+      this.sections = [
         ...generations.reverse().map((generation) => {
           return {
             name: generation.name ?? '',
@@ -61,8 +55,65 @@ export class PokedexSelectDropdownComponent {
                 path: `/pokedex/${versionGroup.identifier}`,
               })),
           };
-        })
-      );
+        }),
+      ];
     });
+  }
+
+  getPokedexVersionGroupsWithPokemon(): void {
+    if (!this.selectedPokemonSpecies) return;
+    forkJoin({
+      versionGroups: this.supabase.getAllVersionGroups(),
+      generations: this.supabase.getAllGenerations(),
+      pokedexVersionGroups: this.supabase.getAllPokedexVersionGroups(),
+      pokemonDexNumbers: this.supabase.getPokemonDexNumbersBySpeciesId(
+        this.selectedPokemonSpecies.id
+      ),
+    }).subscribe(
+      ({
+        versionGroups,
+        generations,
+        pokedexVersionGroups,
+        pokemonDexNumbers,
+      }) => {
+        const versionGroupIds = versionGroups.map((item: any) => item.id);
+        let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
+          (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
+            versionGroupIds.includes(pokedexVersionGroup.version_group_id)
+        );
+        const dexNumbers = pokemonDexNumbers.map((item) => item.pokedex_id);
+        filteredPokedexVersionGroups = filteredPokedexVersionGroups.filter(
+          (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
+            dexNumbers.includes(pokedexVersionGroup.pokedex_id)
+        );
+        const versionGroupsWithPokedexes = new Set(
+          filteredPokedexVersionGroups.map((item) => item.version_group_id)
+        );
+
+        this.sections = [
+          ...generations
+            .reverse()
+            .reduce((acc: DropdownLinkSection[], generation) => {
+              const matchingVersionGroups = versionGroups.filter(
+                (versionGroup): boolean =>
+                  versionGroup.generation_id === generation.id &&
+                  versionGroupsWithPokedexes.has(versionGroup.id)
+              );
+
+              if (matchingVersionGroups.length > 0) {
+                acc.push({
+                  name: generation.name ?? '',
+                  options: matchingVersionGroups.map((versionGroup) => ({
+                    name: versionGroup.name,
+                    path: `/pokedex/${versionGroup.identifier}/${this.selectedPokemonSpecies?.identifier}`,
+                  })),
+                });
+              }
+
+              return acc;
+            }, []),
+        ];
+      }
+    );
   }
 }
