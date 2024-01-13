82226ce390631d62700f4aa1248997336c3dd856
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-01-13 23:22:44 +0000

    implement onChanges for pokedexSelect

--- src/app/features/pokedex/components/pokedex-select-dropdown/pokedex-select-dropdown.component.ts
+++ src/app/features/pokedex/components/pokedex-select-dropdown/pokedex-select-dropdown.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, inject } from '@angular/core';
-import { concatMap, forkJoin, map, tap } from 'rxjs';
+import { Component, Input, OnChanges, inject } from '@angular/core';
+import { Subject, concatMap, forkJoin, map, takeUntil, tap } from 'rxjs';
 import { DropdownLinkSection, Tables } from 'src/app/core/models';
 import { SupabaseService } from 'src/app/core/services/supabase.service';
 
@@ -7,7 +7,7 @@ import { SupabaseService } from 'src/app/core/services/supabase.service';
   selector: 'app-pokedex-select-dropdown',
   templateUrl: './pokedex-select-dropdown.component.html',
 })
-export class PokedexSelectDropdownComponent {
+export class PokedexSelectDropdownComponent implements OnChanges {
   @Input() selectedPokemonSpecies?: Tables<'pokemon_species'>;
   @Input() currentVersionGroupName: string = '';
   @Input() origin: string = 'left';
@@ -17,8 +17,9 @@ export class PokedexSelectDropdownComponent {
   public placeholder: string = 'Select Pokedex';
 
   private supabase: SupabaseService = inject(SupabaseService);
+  private destroy$ = new Subject<void>();
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
     if (this.selectedPokemonSpecies) {
       this.getPokedexVersionGroupsWithPokemon();
     } else {
@@ -31,34 +32,36 @@ export class PokedexSelectDropdownComponent {
       versionGroups: this.supabase.getAllVersionGroups(),
       generations: this.supabase.getAllGenerations(),
       pokedexVersionGroups: this.supabase.getAllPokedexVersionGroups(),
-    }).subscribe(({ versionGroups, generations, pokedexVersionGroups }) => {
-      const versionGroupIds = versionGroups.map((item: any) => item.id);
-      let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
-        (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
-          versionGroupIds.includes(pokedexVersionGroup.version_group_id)
-      );
-      const versionGroupsWithPokedexes = new Set(
-        filteredPokedexVersionGroups.map((item) => item.version_group_id)
-      );
+    })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(({ versionGroups, generations, pokedexVersionGroups }) => {
+        const versionGroupIds = versionGroups.map((item: any) => item.id);
+        let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
+          (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
+            versionGroupIds.includes(pokedexVersionGroup.version_group_id)
+        );
+        const versionGroupsWithPokedexes = new Set(
+          filteredPokedexVersionGroups.map((item) => item.version_group_id)
+        );
 
-      this.sections = [
-        ...generations.reverse().map((generation) => {
-          return {
-            name: generation.name ?? '',
-            options: versionGroups
-              .filter(
-                (versionGroup): boolean =>
-                  versionGroup.generation_id === generation.id &&
-                  versionGroupsWithPokedexes.has(versionGroup.id)
-              )
-              .map((versionGroup) => ({
-                name: versionGroup.name,
-                path: `/pokedex/${versionGroup.identifier}`,
-              })),
-          };
-        }),
-      ];
-    });
+        this.sections = [
+          ...generations.reverse().map((generation) => {
+            return {
+              name: generation.name ?? '',
+              options: versionGroups
+                .filter(
+                  (versionGroup): boolean =>
+                    versionGroup.generation_id === generation.id &&
+                    versionGroupsWithPokedexes.has(versionGroup.id)
+                )
+                .map((versionGroup) => ({
+                  name: versionGroup.name,
+                  path: `/pokedex/${versionGroup.identifier}`,
+                })),
+            };
+          }),
+        ];
+      });
   }
 
   getPokedexVersionGroupsWithPokemon(): void {
@@ -70,51 +73,58 @@ export class PokedexSelectDropdownComponent {
       pokemonDexNumbers: this.supabase.getPokemonDexNumbersBySpeciesId(
         this.selectedPokemonSpecies.id
       ),
-    }).subscribe(
-      ({
-        versionGroups,
-        generations,
-        pokedexVersionGroups,
-        pokemonDexNumbers,
-      }) => {
-        const versionGroupIds = versionGroups.map((item: any) => item.id);
-        let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
-          (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
-            versionGroupIds.includes(pokedexVersionGroup.version_group_id)
-        );
-        const dexNumbers = pokemonDexNumbers.map((item) => item.pokedex_id);
-        filteredPokedexVersionGroups = filteredPokedexVersionGroups.filter(
-          (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
-            dexNumbers.includes(pokedexVersionGroup.pokedex_id)
-        );
-        const versionGroupsWithPokedexes = new Set(
-          filteredPokedexVersionGroups.map((item) => item.version_group_id)
-        );
+    })
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(
+        ({
+          versionGroups,
+          generations,
+          pokedexVersionGroups,
+          pokemonDexNumbers,
+        }) => {
+          const versionGroupIds = versionGroups.map((item: any) => item.id);
+          let filteredPokedexVersionGroups = pokedexVersionGroups.filter(
+            (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
+              versionGroupIds.includes(pokedexVersionGroup.version_group_id)
+          );
+          const dexNumbers = pokemonDexNumbers.map((item) => item.pokedex_id);
+          filteredPokedexVersionGroups = filteredPokedexVersionGroups.filter(
+            (pokedexVersionGroup: Tables<'pokedex_version_groups'>) =>
+              dexNumbers.includes(pokedexVersionGroup.pokedex_id)
+          );
+          const versionGroupsWithPokedexes = new Set(
+            filteredPokedexVersionGroups.map((item) => item.version_group_id)
+          );
 
-        this.sections = [
-          ...generations
-            .reverse()
-            .reduce((acc: DropdownLinkSection[], generation) => {
-              const matchingVersionGroups = versionGroups.filter(
-                (versionGroup): boolean =>
-                  versionGroup.generation_id === generation.id &&
-                  versionGroupsWithPokedexes.has(versionGroup.id)
-              );
+          this.sections = [
+            ...generations
+              .reverse()
+              .reduce((acc: DropdownLinkSection[], generation) => {
+                const matchingVersionGroups = versionGroups.filter(
+                  (versionGroup): boolean =>
+                    versionGroup.generation_id === generation.id &&
+                    versionGroupsWithPokedexes.has(versionGroup.id)
+                );
 
-              if (matchingVersionGroups.length > 0) {
-                acc.push({
-                  name: generation.name ?? '',
-                  options: matchingVersionGroups.map((versionGroup) => ({
-                    name: versionGroup.name,
-                    path: `/pokedex/${versionGroup.identifier}/${this.selectedPokemonSpecies?.identifier}`,
-                  })),
-                });
-              }
+                if (matchingVersionGroups.length > 0) {
+                  acc.push({
+                    name: generation.name ?? '',
+                    options: matchingVersionGroups.map((versionGroup) => ({
+                      name: versionGroup.name,
+                      path: `/pokedex/${versionGroup.identifier}/${this.selectedPokemonSpecies?.identifier}`,
+                    })),
+                  });
+                }
 
-              return acc;
-            }, []),
-        ];
-      }
-    );
+                return acc;
+              }, []),
+          ];
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
