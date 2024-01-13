dd2d9a1503a1d40a6f59f548fef07fa5464edb78
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-01-13 23:19:29 +0000

    stats presented as a table with bst

--- src/app/features/pokedex/components/pokemon-stats/pokemon-stats.component.ts
+++ src/app/features/pokedex/components/pokemon-stats/pokemon-stats.component.ts
@@ -1,52 +1,55 @@
-import { Component, inject, Input, OnChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { SupabaseService } from 'src/app/core/services/supabase.service';
 
 @Component({
   selector: 'app-pokemon-stats',
   templateUrl: './pokemon-stats.component.html',
 })
-export class PokemonStatsComponent implements OnInit, OnChanges {
+export class PokemonStatsComponent implements OnChanges, OnDestroy {
   @Input() pokemon_id: number = 0;
   stats: any = [] as any;
+  total: number = 0;
 
   private supabase: SupabaseService = inject(SupabaseService);
-
-  ngOnInit(): void {
-    this.supabase
-      .getPokemonStatsByPokemonId(this.pokemon_id)
-      .subscribe((data) => {
-        this.stats = data;
-      });
-  }
+  private destroy$ = new Subject<void>();
 
   ngOnChanges() {
     this.supabase
       .getPokemonStatsByPokemonId(this.pokemon_id)
+      .pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
         this.stats = data;
+        this.total = data.reduce(
+          (total: number, stat: any) => total + stat.base_stat,
+          0
+        );
       });
   }
 
-  getProgressBarClass(id: number) {
-    switch (id) {
-      case 1:
-        return 'bg-hp';
-      case 2:
-        return 'bg-attack';
-      case 3:
-        return 'bg-defense';
-      case 4:
-        return 'bg-special_attack';
-      case 5:
-        return 'bg-special_defense';
-      case 6:
-        return 'bg-speed';
-      default:
-        return 'bg-zinc-300';
+  getProgressBarClass(stat: number) {
+    if (stat >= 90) {
+      return 'bg-good_stat';
+    } else if (stat >= 60) {
+      return 'bg-medium_stat';
+    } else {
+      return 'bg-bad_stat';
     }
   }
 
   getProgressBarStyle(base_stat: number) {
     return { width: (base_stat / 255) * 100 + '%' };
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
