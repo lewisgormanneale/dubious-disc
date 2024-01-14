fb078eb1892cb95abea64ac2c3e221e28a65208e
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-01-14 13:08:08 +0000

    Install Angular service worker, font bolding fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/layout/header/header.component';
 import { FooterComponent } from './core/layout/footer/footer.component';
@@ -11,6 +11,7 @@ import { NewsModule } from './features/news/news.module';
 import { GamesModule } from './features/games/games.module';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 const routes: Routes = [
   {
@@ -62,6 +63,12 @@ const routes: Routes = [
     TeamBuilderModule,
     NewsModule,
     GamesModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
