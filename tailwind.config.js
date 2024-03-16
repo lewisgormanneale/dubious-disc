94638cb3e3a4ed0b0b2138990cb418f293bde64a
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-03-16 16:38:28 +0000

    new colour scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        disc: "#702f63",
-        disc_darker: "#522248",
-        dark_primary: "#18181b",
-        dark_secondary: "#09090b",
-        dark_tertiary: "#3a3a3a",
         light_primary: "#f5f5f5",
         light_secondary: "#e5e5e5",
         light_tertiary: "#d5d5d5",
+        dark_primary: "#18181b",
+        dark_secondary: "#09090b",
+        dark_tertiary: "#3a3a3a",
+        porygon_blue: "#4b839c",
+        porygon_blue_darker: "#4a849f",
+        porygon_red: "#ae495b",
+        porygon_red_darker: "#b04a5e",
+        disc: "#3f8eca",
+        disc_darker: "#522248",
         male: "#6890cb",
         female: "#f15d69",
         genderless: "#4fc337",
