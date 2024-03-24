07c22aef9d45d06293f73c2af41906fa9607fd16
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-03-24 12:26:47 +0000

    moving to shared table component wip

--- src/app/features/pokemon/components/pokemon-move-table/pokemon-move-table.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'dd-pokemon-move-table',
-  templateUrl: './pokemon-move-table.component.html',
-})
-export class PokemonMoveTableComponent {
-  @Input() moveGroup: any = [] as any;
-}
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,12 +1,17 @@
 import { Component, Input } from '@angular/core';
 
+interface TableColumn {
+  label: string;
+  property: string;
+}
+
 @Component({
   selector: 'dd-table',
   templateUrl: './table.component.html',
 })
 export class TableComponent {
   @Input() data: any[] = [];
-  @Input() columns: string[] = [];
+  @Input() columns: TableColumn[] = [];
 
   // Add any additional properties or methods here
 }
