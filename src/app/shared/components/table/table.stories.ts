81ebf102d795c5c0f79811551ff900647864bbf6
Lewis Gorman-Neale <lewisgorman-neale@codeweavers.net>
2024-05-17 10:45:55 +0100

    table story arg type added

--- src/app/shared/components/table/table.stories.ts
+++ src/app/shared/components/table/table.stories.ts
@@ -4,11 +4,32 @@ import { TableComponent } from './table.component';
 export default {
   title: 'Shared/Table',
   component: TableComponent,
+  argTypes: {
+    showTableHeaders: {
+      control: { type: 'boolean' },
+    },
+  },
   tags: ['autodocs'],
 } as Meta;
 
 type Story = StoryObj<TableComponent>;
 
-export const Default: Story = {
-  args: {},
+export const ExampleMoveTable: Story = {
+  args: {
+    showTableHeaders: true,
+    columns: [
+      {
+        label: 'Level',
+        property: 'level',
+      },
+      {
+        label: 'Move',
+        property: 'move',
+      },
+      {
+        label: 'Type',
+        property: 'type',
+      },
+    ],
+  },
 };
