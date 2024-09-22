d9ee4aee6956d78e985af247e8b2f1856bc1d97b
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-09-22 15:00:21 +0100

    image spinner on initial load, background image opacity lowered

--- src/app/features/pokemon/components/pokemon-summary/pokemon-summary.component.ts
+++ src/app/features/pokemon/components/pokemon-summary/pokemon-summary.component.ts
@@ -15,13 +15,14 @@ export class PokemonSummaryComponent implements OnChanges {
   imageUrl: string = '';
   shiny: boolean = false;
   female: boolean = false;
-  isLoading: boolean = true;
+  imageLoaded: boolean = false;
 
   private supabase: SupabaseService = inject(SupabaseService);
   private destroy$ = new Subject<void>();
 
   ngOnChanges(): void {
     if (!this.selectedForm.id) return;
+    this.imageLoaded = false;
     this.supabase
       .getPokemonTypesByPokemonId(this.selectedForm.id)
       .subscribe((data) => {
@@ -40,26 +41,44 @@ export class PokemonSummaryComponent implements OnChanges {
     this.imageUrl = this.supabase.storage
       .from('pokemon')
       .getPublicUrl(url).data.publicUrl;
+    this.loadImage();
   }
 
-  onImageLoad() {
-    this.isLoading = false;
+  loadImage() {
+    const img = new Image();
+    img.src = this.imageUrl;
+    img.onload = () => {
+      this.imageLoaded = true;
+    };
+    img.onerror = () => {
+      this.imageLoaded = true;
+      this.imageUrl = 'assets/images/question-icon.png';
+    };
   }
 
-  getTypeBackgroundColor() {
+  getTypeBackgroundColor(opacity: number = 0.3): string {
+    const hexToRgba = (hex: string, alpha: number = 1) => {
+      hex = hex.replace('#', '');
+      const r = parseInt(hex.substring(0, 2), 16);
+      const g = parseInt(hex.substring(2, 4), 16);
+      const b = parseInt(hex.substring(4, 6), 16);
+      return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+    };
+
     if (this.pokemonTypes && this.pokemonTypes.length > 1) {
-      return (
-        'linear-gradient(to bottom right, #' +
-        this.pokemonTypes[0].type_id.darker_type_color +
-        ',' +
-        '#' +
-        this.pokemonTypes[1].type_id.darker_type_color +
-        ')'
+      const color1 = hexToRgba(
+        this.pokemonTypes[0].type_id.type_color,
+        opacity
       );
+      const color2 = hexToRgba(
+        this.pokemonTypes[1].type_id.type_color,
+        opacity
+      );
+      return `linear-gradient(to bottom right, ${color1}, ${color2})`;
     } else if (this.pokemonTypes && this.pokemonTypes.length == 1) {
-      return '#' + this.pokemonTypes[0].type_id.darker_type_color;
+      return hexToRgba(this.pokemonTypes[0].type_id.type_color, opacity);
     } else {
-      return 'linear-gradient(to bottom right, #27272a, #27272a)';
+      return `rgba(39, 39, 42, ${opacity})`;
     }
   }
 
